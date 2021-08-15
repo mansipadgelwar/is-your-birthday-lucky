4193549f62bf9ed17a1ae636310a3aa77068f45d
mansipadgelwar <mpadgelwar@gmail.com>
2021-08-15 10:33:07 +0530

    feat: add GIFs

--- app.js
+++ app.js
@@ -2,23 +2,31 @@ var date = document.querySelector("#date");
 var luckyNumber = document.querySelector("#lucky-number");
 var btnSubmit = document.querySelector("#btn-submit");
 var btnReset = document.querySelector("#btn-reset");
+var output = document.querySelector("#output");
+var happyOutputImage = document.querySelector("#happy-output-img");
+var sadOutputImage = document.querySelector("#sad-output-img");
 btnSubmit.addEventListener("click",clickHandler);
 
 function clickHandler(e){
     e.preventDefault();
+
+    happyOutputImage.style.display = "none";
+    sadOutputImage.style.display = "none";
+    output.innerText = "";
+
     const lucky = Number(luckyNumber.value);
     const dateInput = date.value.split("-").join("");
     let sum = 0;
 
     if(date.value === "")
     {
-        output.innerHTML = "Please enter your birth date";
+        output.innerText = "Please enter your birth date";
     }
     else
     {
         if(lucky <= 0 || lucky === "")
         {
-            output.innerHTML = "Please enter valid lucky number.";
+            output.innerText = "Please enter valid lucky number.";
         }
         else
         {
@@ -29,11 +37,14 @@ function clickHandler(e){
             }      
             if(sum % lucky === 0)
             {
-                output.innerHTML = "🎉🎉Hurray! Your birthdate is a lucky number.";
+                output.innerText = "🎉🎉Hurray! Your birthdate is a lucky number.";
+                happyOutputImage.style.display = "block";
+                
             }
             else
             {
-             output.innerHTML = "😥😥Aah! Your birthday is not a lucky number.";
+             output.innerText = "😥😥Aah! Your birthday is not a lucky number.";
+             sadOutputImage.style.display = "block";             
             }
         }
     }
@@ -43,6 +54,9 @@ function clickHandler(e){
 btnReset.addEventListener("click",function (){
    date.value = "";
    output.innerText = "";
-   luckyNumber.value = ""; 
+   luckyNumber.value = "";
+   happyOutputImage.style.display = "none";
+   sadOutputImage.style.display = "none"; 
+   
 });
 
